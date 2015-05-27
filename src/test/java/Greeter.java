1c43ad96552da9164c35bbcdc3a93924d600d86b
chcat <psalimov@excelsior-usa.com>
2015-05-27 12:02:16 +0700

    Test spock integration.

---
+++ src/test/java/Greeter.java
@@ -0,0 +1,21 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by pasa on 26.05.2015.
+ */
+
+@Controller
+@RequestMapping("greeter")
+public interface Greeter {
+
+    @ResponseBody
+    @RequestMapping("greetings/{language}")
+    String getGreeting(@PathVariable String language,
+                       @RequestParam(defaultValue = "Anonimous") String name);
+
+}
+
