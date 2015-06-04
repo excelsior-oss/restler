1c5f2d1bc33b4ec63642b3ad9420db9f63928c1d
Aleksey Zhidkov <aleksey.zhidkov@gmail.co>
2015-06-04 16:08:22 +0600

    Added throwing of HttpExecutionException with response body, when no other message converter cannot convert response

--- restler-core/src/main/java/org/restler/http/SimpleHttpRequestExecutor.java
+++ restler-core/src/main/java/org/restler/http/SimpleHttpRequestExecutor.java
@@ -1,21 +1,71 @@
 package org.restler.http;
 
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestOperations;
+import org.restler.util.Util;
+import org.springframework.http.*;
+import org.springframework.http.converter.GenericHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleHttpRequestExecutor implements HttpRequestExecutor {
 
-    private final RestOperations restOperations;
+    private final RestTemplate restTemplate;
 
-    public SimpleHttpRequestExecutor(RestOperations restOperations) {
-        this.restOperations = restOperations;
+    public SimpleHttpRequestExecutor(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+        restTemplate.getMessageConverters().add(new BodySavingMessageConverter());
     }
 
     public <T> ResponseEntity<T> execute(ExecutableRequest<T> executableRequest) {
         RequestEntity<?> requestEntity = executableRequest.toRequestEntity();
-
-        return restOperations.exchange(requestEntity, executableRequest.getReturnType());
+        return restTemplate.exchange(requestEntity, executableRequest.getReturnType());
     }
 
+    private class BodySavingMessageConverter implements GenericHttpMessageConverter<Object> {
+        @Override
+        public boolean canRead(Type type, Class<?> contextClass, MediaType mediaType) {
+            return true;
+        }
+
+        @Override
+        public Object read(Type type, Class<?> contextClass, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+            return throwException(inputMessage);
+        }
+
+        @Override
+        public boolean canRead(Class<?> clazz, MediaType mediaType) {
+            return true;
+        }
+
+        @Override
+        public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+            return true;
+        }
+
+        @Override
+        public List<MediaType> getSupportedMediaTypes() {
+           return new ArrayList<>();
+        }
+
+        @Override
+        public Object read(Class<?> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+            return throwException(inputMessage);
+        }
+
+        private Object throwException(HttpInputMessage inputMessage) throws IOException {
+            String responseBody = Util.toString(inputMessage.getBody());
+            inputMessage.getBody().close();
+            throw new HttpExecutionException(responseBody);
+        }
+
+        @Override
+        public void write(Object o, MediaType contentType, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+
+        }
+    }
 }
