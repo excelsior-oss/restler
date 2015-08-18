30ce06d415e7ba8b8fa743debe94f2ce34b91cd9
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2015-08-18 16:56:03 +0600

    Minor reformat code

--- restler-core/src/test/groovy/org/restler/client/ControllerMethodInvocationMapperTest.groovy
+++ restler-core/src/test/groovy/org/restler/client/ControllerMethodInvocationMapperTest.groovy
@@ -5,10 +5,13 @@ import spock.lang.Specification
 
 class ControllerMethodInvocationMapperTest extends Specification {
 
+    def anyUri = null
+
     def "Controller method invocation mapper should correctly process null arguments"() {
         given:
-        def mapper = new ControllerMethodInvocationMapper("any url")
+        def mapper = new ControllerMethodInvocationMapper(anyUri, ParameterResolver.valueOfParamResolver())
         def method = Greeter.class.getDeclaredMethod("getGreeting", [String.class, String.class] as Class[])
+
         when:
         def invocation = mapper.apply(method, null, null)
 
