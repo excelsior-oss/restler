7eb7b459013a40cd6ae03a2a136d32da6b561265
Oleg Rudenko <olegarx008@gmail.com>
2016-02-18 16:46:31 +0600

    Deleted useles methods from ResourceProxy.

--- restler-spring-data-rest/src/main/java/org/restler/spring/data/methods/CrudMethod.java
+++ restler-spring-data-rest/src/main/java/org/restler/spring/data/methods/CrudMethod.java
@@ -8,9 +8,6 @@ import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * Created by rudenko on 11.02.2016.
- */
 public interface CrudMethod {
     boolean isCrudMethod(Method method);
 
