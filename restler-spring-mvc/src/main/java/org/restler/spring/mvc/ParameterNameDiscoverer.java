d297677294cfd8f409a916f83704ef9af5931fce
Oleg Rudenko <olegarx008@gmail.com>
2016-03-14 16:04:39 +0600

    Remove unused code for more readable.

--- restler-spring-mvc/src/main/java/org/restler/spring/mvc/ParameterNameDiscoverer.java
+++ restler-spring-mvc/src/main/java/org/restler/spring/mvc/ParameterNameDiscoverer.java
@@ -1,6 +1,8 @@
 package org.restler.spring.mvc;
 
 import jdk.internal.org.objectweb.asm.ClassReader;
+import org.restler.client.RestlerException;
+import org.restler.spring.mvc.asm.ClassUtils;
 import org.restler.spring.mvc.asm.ParameterNameDiscoveringVisitor;
 
 import java.io.IOException;
@@ -44,57 +46,18 @@ public class ParameterNameDiscoverer {
     }
 
     private String[] getParameterNamesFromLocalVariableTable(Method method) {
-//        Method originalMethod = findBridgedMethod(method);
-        Method originalMethod = method;
-        Class<?> declaringClass = originalMethod.getDeclaringClass();
+        Class<?> declaringClass = method.getDeclaringClass();
 
         Map<Member, String[]> map = inspectClass(declaringClass);
 
         if (map != null) {
-            return map.get(originalMethod);
+            return map.get(method);
         }
         return null;
     }
 
-//    private Method findBridgedMethod(Method bridgeMethod) {
-//        if(bridgeMethod == null || !bridgeMethod.isBridge()) {
-//            return bridgeMethod;
-//        }
-//
-//        List<Method> methods = new ArrayList<>();
-//
-//        getAllMethods(bridgeMethod.getDeclaringClass(), methods);
-//
-//        List<Method> candidates = methods.stream().filter(method -> isBridgedCandidate(method, bridgeMethod)).collect(Collectors.toList());
-//
-//
-//
-//
-//        return candidates.get(0);
-//    }
-//
-//    private void getAllMethods(Class<?> clazz, List<Method> result) {
-//        for(Method method : clazz.getDeclaredMethods()) {
-//            result.add(method);
-//        }
-//
-//        if(clazz.getSuperclass() != null) {
-//            getAllMethods(clazz.getSuperclass(), result);
-//        } else if(clazz.isInterface()) {
-//            for(Class<?> interf : clazz.getInterfaces()) {
-//                getAllMethods(interf, result);
-//            }
-//        }
-//    }
-//
-//    private boolean isBridgedCandidate(Method candidateMethod, Method bridgeMethod) {
-//        return (!candidateMethod.isBridge() && !candidateMethod.equals(bridgeMethod) &&
-//                candidateMethod.getName().equals(bridgeMethod.getName()) &&
-//                candidateMethod.getParameterTypes().length == bridgeMethod.getParameterTypes().length);
-//    }
-
     private Map<Member, String[]> inspectClass(Class<?> clazz) {
-        InputStream inputStream = clazz.getResourceAsStream(getClassFileName(clazz));
+        InputStream inputStream = clazz.getResourceAsStream(ClassUtils.getClassFileName(clazz));
         if (inputStream == null) {
             return null;
         }
@@ -104,9 +67,13 @@ public class ParameterNameDiscoverer {
             classReader.accept(new ParameterNameDiscoveringVisitor(clazz, map), 0);
             return map;
         }
-        catch (IOException | IllegalArgumentException ex) {
-            // ignore
-        } finally {
+        catch (IOException e) {
+            throw new RestlerException("Can't read class from stream.", e);
+        }
+        catch(IllegalArgumentException e) {
+            throw new RestlerException("Illegal argument.", e);
+        }
+        finally {
             try {
                 inputStream.close();
             }
@@ -114,11 +81,5 @@ public class ParameterNameDiscoverer {
                 // ignore
             }
         }
-        return null;
-    }
-
-    private String getClassFileName(Class<?> clazz) {
-        String name = clazz.getName();
-        return name.substring(name.lastIndexOf('.')+1) + ".class";
     }
 }
