3bb34d70f07f2fa65366381822e22482a495464e
Oleg Rudenko <olegarx008@gmail.com>
2016-04-20 13:33:09 +0600

    Change cycles to streams.

--- restler-spring-mvc/src/main/java/org/restler/spring/mvc/AnnotationUtils.java
+++ restler-spring-mvc/src/main/java/org/restler/spring/mvc/AnnotationUtils.java
@@ -2,6 +2,7 @@ package org.restler.spring.mvc;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,33 +13,14 @@ public class AnnotationUtils {
     }
 
     public static boolean isAnnotated(Method method, Class<? extends Annotation> annotationClass) {
-        for(Annotation annotation : method.getDeclaredAnnotations()) {
-            if(findAnnotation(annotation.annotationType(), annotationClass, new HashSet<>()) != null) {
-                return true;
-            }
-        }
-
-        return false;
+        return Arrays.stream(method.getDeclaredAnnotations()).
+                map((Annotation annotation)->findAnnotation(annotation.annotationType(), annotationClass, new HashSet<>())).
+                filter((Annotation annotation)->annotation != null).
+                count() > 0;
     }
 
     public static <R extends Annotation> R getAnnotation(Class<?> clazz, Class<R> annotationClass) {
         return (R)findAnnotation(clazz, annotationClass, new HashSet<>());
-    }
-
-    public static <R extends Annotation> R getAnnotation(Method method, Class<R> annotationClass) {
-        Annotation resultAnnotation;
-        for(Annotation annotation : method.getDeclaredAnnotations()) {
-
-            if(annotation.annotationType().equals(annotationClass)) {
-                return (R)annotation;
-            }
-
-            if((resultAnnotation = findAnnotation(annotation.annotationType(), annotationClass, new HashSet<>())) != null) {
-                return (R)resultAnnotation;
-            }
-        }
-
-        return null;
     }
 
     private static Annotation findAnnotation(Class<?> clazz, Class<? extends Annotation> annotationClass, Set<Class<?>> set) {
@@ -54,23 +36,30 @@ public class AnnotationUtils {
             return result;
         }
 
-        Class<?>[] interfaces = clazz.getInterfaces();
+        Annotation findResult = Arrays.stream(clazz.getInterfaces()).
+                map((Class<?> interf)->findAnnotation(interf, annotationClass, set)).
+                filter((Annotation annotation)->annotation != null).
+                findFirst().
+                orElse(null);
 
-        for(Class<?> interf : interfaces) {
-            if(( result = findAnnotation(interf, annotationClass, set)) != null) {
-                return result;
-            }
+        if(findResult != null) {
+            return findResult;
         }
 
-        for(Annotation annotation : clazz.getDeclaredAnnotations()) {
-            if(annotation.annotationType().equals(annotationClass)) {
-                return annotation;
-            } else if ((result = findAnnotation(annotation.annotationType(), annotationClass, set)) != null) {
-                return result;
-            }
+        Annotation interfaceAnnotation = Arrays.stream(clazz.getDeclaredAnnotations()).
+                filter((Annotation annotation)->annotation.annotationType().equals(annotationClass)).
+                findFirst().
+                orElse(null);
+
+        if(interfaceAnnotation != null) {
+            return interfaceAnnotation;
         }
 
-        return null;
+        return Arrays.stream(clazz.getDeclaredAnnotations()).
+                map((Annotation annotation)->findAnnotation(annotation.annotationType(), annotationClass, set)).
+                filter((Annotation annotation)->annotation != null).
+                findFirst().
+                orElse(null);
     }
 
 }
